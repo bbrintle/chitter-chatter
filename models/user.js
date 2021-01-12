3c6a7ac7daa6f1a0d4f49d7d56b198eaf71d62a8
mhans003 <michaeledwardhanson@gmail.com>
2021-01-11 22:13:07 -0800

    Started process of adding passport for authentication on the server side: Configured passport using JWTs and using a secret key to establish authorization; Routes for /auth created in server.js which will create a new user or authenticate and existing user and send back an appropriate Message object (with message body and error status); Capitalized model file names.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
+
 // !update email and password for authentification
 const userSchema = mongoose.Schema({ 
     username: String,
@@ -9,5 +11,34 @@ const userSchema = mongoose.Schema({
     role: String
     
   });
+
+//Hash the password before saving it. 
+userSchema.pre("save", function(next) {
+  if(!this.isModified("password")) {
+      return next();
+  }
+  //Hash the password using bcrypt.
+  bcrypt.hash(this.password, 10, (error, hashedPassword) => {
+      if(error) {
+          return next(error);
+      }
+      this.password = hashedPassword;
+      next();
+  });
+});
+
+userSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, (error, isMatch) => {
+      if(error) {
+          return callback(error);
+      } else {
+          if(!isMatch) {
+              return callback(null, isMatch);
+          }
+          //Attach the user object with the request object.
+          return callback(null, this);
+      } 
+  });
+}
 
 export default mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import Message from "./models/message.js"
+import Message from "./models/Message.js"
 // import routes from "./routes";
+
+//Import Passport and JWT
+import passport from "passport";
+import JWT from "jsonwebtoken";
+
+//Include user model
+import User from "./models/User.js";
 
 const app = express();
 // const passport = require("./config/passport");
@@ -42,6 +49,128 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/communitychatap
 // Need html route for going to profile
 
 // Routes for data (i.e. messages, recognition posts, comments on posts (sub-part)
+
+//-----AUTH ROUTES----
+
+//When logging in, use the user ID to return a JWT token.
+const signToken = userID => {
+  return JWT.sign({
+      //Set issuer to our secret; Set subject to our user name.
+      iss: process.env.JWT_SECRET,
+      sub: userID
+  }, process.env.JWT_SECRET, {expiresIn: "1hr"});
+};
+
+//Register a new user.
+app.post("/auth/register", (request, response) => {
+  //Pull out the credential information from the request body.
+  const { username, password, email } = request.body;
+  //See if this username exists.
+  User.findOne({username}, (error, user) => {
+      if(error) {
+          //If there was an error, indicate that something went wrong.
+          response.status(500).json(
+              {
+                  message: {
+                      msgBody: "An error occured while searching for username.", 
+                      msgError: true
+                  }
+              }
+          );
+      }
+      if(user) {
+          //If the user exists (and therefore user object is returned):
+          response.status(400).json(
+              {
+                  message: {
+                      msgBody: "An error occured", 
+                      msgError: true
+                  }
+              }
+          );
+      } else {
+          //Otherwise, create the user using the new credentials.
+          const newUser = new User({email, username, password});
+          console.log(newUser);
+          newUser.save(error => {
+              if(error) {
+                  console.log(error);
+                  response.status(500).json(
+                      {
+                          message: {
+                              msgBody: "An error occured while registering user.", 
+                              msgError: true
+                          }
+                      }
+                  );
+              } else {
+                  response.status(200).json(
+                      {
+                          message: {
+                              msgBody: "Successfully registered new user.",
+                              msgError: false
+                          }
+                      }
+                  )
+              }
+          });
+      }
+  })
+});
+
+//Log an existing user in (must authenticate).
+app.post("/auth/login", passport.authenticate("local", {session: false}), (request, response) => {
+  if(request.isAuthenticated()) {
+      //If the user is authenticated, pull out the credentials of that user.
+      const { _id, username } = request.user;
+      //Create a JWT token since we have signed in.
+      const token = signToken(_id);
+      //Using the JWT token, set cookie and send authorization.
+      response.cookie("access_token", token, {httpOnly: true, sameSite: true});
+      response.status(200).json(
+          {
+              isAuthenticated: true, 
+              user: { username }
+          }
+      );
+  } else {
+      response.status(401).json(
+          {
+              message: {
+                  msgBody: "Incorrect username or password.",
+                  msgError: true
+              }
+          }
+      );
+  }
+});
+
+//Log a user out using the associated access token.
+app.get("/auth/logout", passport.authenticate("jwt", {session: false}), (request, response) => {
+  response.clearCookie("access_token");
+  response.json(
+      {
+          user: {
+              username: ""
+          }, 
+          success: true
+      }
+  );
+});
+
+//Allow the user to remain authenticated.
+app.get("/auth/authenticated", passport.authenticate("jwt", {session: false}), (request, response) => {
+  const { username } = request.user;
+  response.status(200).json(
+      {
+          isAuthenticated: true,
+          user: { username }
+      }
+  );
+});
+
+//-----
+
 
 // GET route for getting all messages
 app.get("/api/messages/all", function(req, res) {
