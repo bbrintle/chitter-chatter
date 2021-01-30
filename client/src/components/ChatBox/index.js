424905b475fe4f4844f9cd11ed99d942d6f5f1c0
areye022 <areye022@gmail.com>
2021-01-30 12:16:38 -0800

    adjusted use effects, messages and chatrooms now matching, and do not revert back to previous chats

--- client/src/components/ChatBox/index.js
+++ client/src/components/ChatBox/index.js
@@ -1,26 +1,68 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import ContainerFluid from "../ContainerFluid";
 import Chat from "../Chat";
 import Pusher from "pusher-js";
-import { RoomContext } from "../../Context/RoomContext";
 
 //Include the Message Service.
-import axios from "axios"
-import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const ChatBox = (props) => {
+  // Get chat room id from params
 
-    const roomContext = useContext(RoomContext);
+  const { chatroomID } = useParams();
 
-    useEffect(() => {
-      roomContext.getMessages();
-    }, [roomContext.currentRoomID]);
+  // chat room name is not part of params so get from props (params would be cleaner)
+  // const { chatroomName } = props;
+  // set initial state of messages
+  const [messages, setMessages] = useState([]);
 
-    return (
-        <ContainerFluid>
-            <Chat/>
-        </ContainerFluid>
-    );
-}
+  
+
+  useEffect(() => {
+  // I wrote an async api call function inside of useEffect to help with scoping
+  async function getMessages() {
+    const results = await axios.get(`/api/messages/${chatroomID}`);
+    setMessages(results.data);
+  }
+    
+    // call function to get messages, this will get called whenever the chatroom id is changed
+    getMessages();
+  }, [chatroomID, messages]);
+
+  // function to setPusher for new chat room
+  const setPusher = () => {
+    const pusher = new Pusher("02315d0fbb0283ef5f14", {
+      cluster: "us3",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessages) => {
+      console.log("Test pusher");
+      console.log(chatroomID, "pusher triggered");
+    });
+    //Unbind and Unsubscribe to prevent multiple connections to pusher and insure that there is only one subscriber at a time
+    return () => {
+      // getMessages() future use, may move getMessages out of useEffect
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  };
+  //This function, when called, will execute setPusher()
+  function handlePusher() {
+    setPusher();
+  }
+  // Chatroom id was removed as we can get it from useParams
+  return (
+    <ContainerFluid>
+      <Chat
+        messages={messages}
+        handlePusher={handlePusher}
+        // getMessages ={getMessages}  this is just a thought for now
+        // chatroomName={chatroomName}  no longer needed as the useParams will do this in the chat component
+      />
+    </ContainerFluid>
+  );
+};
 
 export default ChatBox;
