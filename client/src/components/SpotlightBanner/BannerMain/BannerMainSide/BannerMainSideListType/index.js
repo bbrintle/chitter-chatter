9ce2f589ced7905e5659cccb025bf14212196512
mhans003 <michaeledwardhanson@gmail.com>
2021-01-31 14:36:32 -0800

    Added a component folder for customizable spotlight banner content (eventually customizable using base components), which will always appear at the top of the Dashboard component. Added two types of banner header components and one type of sidebar header components within existing grid system components. Placeholder content included.

---
+++ client/src/components/SpotlightBanner/BannerMain/BannerMainSide/BannerMainSideListType/index.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+const BannerMainSideListType = (props) => {
+    return (
+        <div className="banner-main-side-list-type px-3 px-md-4 py-3 py-xl-4 mt-3 mt-sm-0">
+            <div className="heading-title">
+                <div className="mb-1">{props.headingTitle}</div>
+            </div>
+            <div className="heading-items">
+                <ul className="mt-2">
+                    {props.headingItems ? props.headingItems.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    }) : null}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default BannerMainSideListType;
