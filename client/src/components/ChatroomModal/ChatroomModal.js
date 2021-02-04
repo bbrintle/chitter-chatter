b65fda397801542c864fda96ef941fa9b8b0317c
bbrintle <bbrintle@gmail.com>
2021-02-03 21:02:19 -0800

    update sidebar to show contacts in modal

--- client/src/components/ChatroomModal/ChatroomModal.js
+++ client/src/components/ChatroomModal/ChatroomModal.js
@@ -1,13 +1,15 @@
 import { Button, ListGroup } from "react-bootstrap";
 import axios from "axios"
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Modal from "react-bootstrap/Modal"
+import { AuthContext } from "../../Context/AuthContext";
 
 const ChatroomModal = (props) => {
     const [chatrooms, setChatrooms] = useState([]);
     const [show, setShow] = useState(false);
     const [input, setInput] = useState("");
     const handleClose = () => setShow(false);
+    const authContext = useContext(AuthContext);
     setShow(props.show)
 
 
@@ -39,11 +41,18 @@ const ChatroomModal = (props) => {
         <>
             <Modal show={show} onHide={handleClose} animation={false}>
                 <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>New Heading</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     
                     <form>
+                        {AuthContext.user}
+                        {AuthContext.user.contacts.map((contact, index) => (
+                            <p key={index} >
+                                {contact.username}
+                            </p>
+                        ))}
+
                         <label>Please provide a name for the chatroom:</label>
                         <input 
                             value={input} 
